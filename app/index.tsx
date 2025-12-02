ccfe0aed4ad8a15f70b45f9e07cd61f58272539c
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-12-02 09:22:38 +0530

    initial Expo Router setup

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Khan Shoaib</Text>
+      <Link href="/about">Visit About Screen</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+})
