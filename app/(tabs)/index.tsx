a5790a31bb0e009ff98538b6b434159c5f15f7f3
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2025-12-02 20:33:16 +0530

    Initialize Convex client, theme provider, and basic todo list functionality on the index screen.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,26 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { Link } from "expo-router";
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Index() {
   const {toggleDarkMode} = useTheme()
+  const todos = useQuery(api.todos.getTodos)
+  const addTodo = useMutation(api.todos.addTodo)
+  const clearAllTodos = useMutation(api.todos.clearAllTodos)
 
   return (
     <View style={styles.container}>
       <Text>Khan Shoaib</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle Theme</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => addTodo({text: "Hit the gym"})}>
+        <Text>Add Todo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => clearAllTodos()}>
+        <Text>Clear All Todos</Text>
       </TouchableOpacity>
     </View>
   );
